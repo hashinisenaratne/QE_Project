761f18f8e9a489a0d09abfac1f9acecc92ddb47c
Laksheen <cresclux@yahoo.com>
2015-03-10 12:02:31 +0530

    test classes

---
+++ Checkers-master/src/Logic/HeuristicUtilTest.java
@@ -0,0 +1,44 @@
+package Logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HeuristicUtilTest {
+
+  @Test
+  public void testCalcHeuristicValue() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testCalcHeuristicValueEnding() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testIsUsedByNormalPiece() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testIsUsedByQueen() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testIsUsed() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testPieceCount() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testIsOnlyKings() {
+    fail("Not yet implemented");
+  }
+
+}
