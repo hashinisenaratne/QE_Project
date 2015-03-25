ad5f5fed21c9080ef9daf9b63eeb0058ae3c39f9
Hashini Senaratne <hashz1990@gmail.com>
2015-03-25 18:52:09 +0530

    adding cobertura test coverage

--- CheckersGame/src/test/java/com/checkers/Logic/HeuristicUtilTest.java
+++ CheckersGame/src/test/java/com/checkers/Logic/HeuristicUtilTest.java
@@ -6,39 +6,39 @@ import org.junit.Test;
 
 public class HeuristicUtilTest {
 
-  @Test
-  public void testCalcHeuristicValue() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testCalcHeuristicValueEnding() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testIsUsedByNormalPiece() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testIsUsedByQueen() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testIsUsed() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testPieceCount() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testIsOnlyKings() {
-    fail("Not yet implemented");
-  }
+//  @Test
+//  public void testCalcHeuristicValue() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testCalcHeuristicValueEnding() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testIsUsedByNormalPiece() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testIsUsedByQueen() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testIsUsed() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testPieceCount() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testIsOnlyKings() {
+//    fail("Not yet implemented");
+//  }
 
 }
