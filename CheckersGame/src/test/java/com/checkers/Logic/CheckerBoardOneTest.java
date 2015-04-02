c13a6cb73a8c536be3d7bee73c8cc3f3ffb5e96a
Laksheen <cresclux@yahoo.com>
2015-04-02 13:41:09 +0530

    completed testIsMoveable

--- CheckersGame/src/test/java/com/checkers/Logic/CheckerBoardOneTest.java
+++ CheckersGame/src/test/java/com/checkers/Logic/CheckerBoardOneTest.java
@@ -2,6 +2,7 @@ package com.checkers.Logic;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -16,6 +17,12 @@ public class CheckerBoardOneTest {
   
   private char[][] customCheckersBoardInstance;
   private CheckerBoard boardforTest;
+  char typeR = 'r';
+  char typeB = 'b';
+  char empty = '_';
+  char invalid = '#';
+  char rKing = 'R';
+  char bKing = 'B';
 
   @BeforeClass
   public static void setUpClass() {
@@ -28,7 +35,7 @@ public class CheckerBoardOneTest {
   @Before
   public void setUp() {
       boardforTest = new CheckerBoard(8);
-      setCustomCheckerBoard();
+    resetCustomCheckerBoard();
       boardforTest.setCheckersBoard(customCheckersBoardInstance);
   }
 
@@ -39,7 +46,7 @@ public class CheckerBoardOneTest {
   /**
    * Initialize a custom checker board for testing
    */
-  private void setCustomCheckerBoard() {
+  private void resetCustomCheckerBoard() {
       int boardSize = 8;
       customCheckersBoardInstance = new char[boardSize][boardSize];
       char typeR = 'r';
@@ -97,9 +104,9 @@ public class CheckerBoardOneTest {
   public void testIsMoveable() {
 
     // test for different invalid cell combinations
-    assertFalse("Invalid cells are accepted", boardforTest.isMoveable(7, 2, 6, 3));
-    assertFalse("Invalid cells are accepted", boardforTest.isMoveable(7, 2, 5, 3));
-    assertFalse("Invalid cells are accepted", boardforTest.isMoveable(7, 1, 6, 3));
+    assertFalse("Invalid cells are accepted", boardforTest.isMoveable(7, 2, 6, 2));
+    assertFalse("Invalid cells are accepted", boardforTest.isMoveable(6, 0, 1, 4));
+    assertFalse("Invalid cells are accepted", boardforTest.isMoveable(5, 2, 6, 3));
 
     // test for impossible destination rows
     assertFalse("Invalid cells are accepted", boardforTest.isMoveable(7, 1, 9, 3));
@@ -110,7 +117,38 @@ public class CheckerBoardOneTest {
     assertFalse("Invalid cells are accepted", boardforTest.isMoveable(7, 1, 5, -3));
 
     // test for empty source cell
-    assertFalse("Invalid cells are accepted", boardforTest.isMoveable(3, 1, 6, 2));
+    customCheckersBoardInstance[3][1] = empty;
+    assertFalse("Source cell is empty, no piece to move", boardforTest.isMoveable(3, 1, 6, 2));
+
+    // test for not empty destination cell, from a not empty source cell
+    assertFalse("Destination cell is occupied, can't move", boardforTest.isMoveable(0, 2, 2, 4));
+
+    // test for a move in the same column
+    assertFalse("Can't move in the same column", boardforTest.isMoveable(1, 3, 3, 3));
+
+    // test for a red piece to move backward
+    customCheckersBoardInstance[1][1] = empty;
+    assertFalse("Piece cannot move backward", boardforTest.isMoveable(2, 2, 1, 1));
+
+    // test for a black piece to move backward
+    customCheckersBoardInstance[6][6] = empty;
+    assertFalse("Piece cannot move backward", boardforTest.isMoveable(5, 7, 6, 6));
+
+    // test for a larger move for both pieces
+    assertFalse("Can't allow large moves", boardforTest.isMoveable(2, 2, 6, 6));
+    assertFalse("Can't allow large moves", boardforTest.isMoveable(5, 5, 3, 1));
+
+    // test for 1 cell moves
+    assertTrue("Valid 1 move cell", boardforTest.isMoveable(2, 2, 3, 3));
+    assertTrue("Valid 1 move cell", boardforTest.isMoveable(7, 7, 6, 6));
+
+    // test for valid cutting pieces
+    assertFalse("A red piece can't cut a red piece", boardforTest.isMoveable(1, 3, 3, 5));
+    customCheckersBoardInstance[3][3] = typeB;
+    assertTrue("A red piece can cut a black piece", boardforTest.isMoveable(2, 2, 4, 4));
+    assertFalse("A black piece can't cut a black piece", boardforTest.isMoveable(6, 4, 4, 6));
+    customCheckersBoardInstance[4][6] = typeR;
+    assertTrue(boardforTest.isMoveable(5, 7, 3, 5));
 
   }
   /*
@@ -130,20 +168,5 @@ public class CheckerBoardOneTest {
    * @Test public void testMovePieceByType() { fail("Not yet implemented"); }
    * @Test public void testHasMovesIntInt() { fail("Not yet implemented"); }
    * @Test public void testHasMovesChar() { fail("Not yet implemented"); }
-   * @Test public void testMove() { fail("Not yet implemented"); }
-   * @Test public void testHasCutsIntInt() { fail("Not yet implemented"); }
-   * @Test public void testHasCutsCharArrayArrayIntInt() { fail("Not yet implemented"); }
-   * @Test public void testHasMoreCuts() { fail("Not yet implemented"); }
-   * @Test public void testCutPieceByType() { fail("Not yet implemented"); }
-   * @Test public void testCanMove() { fail("Not yet implemented"); }
-   * @Test public void testGetAllCaptures() { fail("Not yet implemented"); }
-   * @Test public void testGetAllNonCaptures() { fail("Not yet implemented"); }
-   * @Test public void testCloneArray() { fail("Not yet implemented"); }
-   * @Test public void testGetMoveFromMinMax() { fail("Not yet implemented"); }
-   * @Test public void testInvertColour() { fail("Not yet implemented"); }
-   * @Test public void testFindValue() { fail("Not yet implemented"); }
-   * @Test public void testCalcHeuristic() { fail("Not yet implemented"); }
-   * @Test public void testEval() { fail("Not yet implemented"); }
-   * @Test public void testReset() { fail("Not yet implemented"); }
    */
 }
